3c2b515ee73663961518967796243ed86e00b55b
sasaki5963 <masato11119@gmail.com>
2020-10-22 18:08:54 +0900

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -2,9 +2,9 @@ import random
 
 deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13] * 4
 
-hand = []
 
 def deal():
+  hand = []
   for i in range(2):
     random.shuffle(deck)
     card = deck.pop()
@@ -19,10 +19,23 @@ def deal():
     hand.append(card)
   return hand
 
-deal()
-print(hand)
 
-def total():
+def hit(hand):
+  random.shuffle(deck)
+  card = deck.pop()
+  if card == 11:
+    card = "J"
+  if card == 12:
+    card = "Q"
+  if card == 11:
+    card = "K"
+  if card == 1:
+    card = "A"
+  hand.append(card)
+  return hand
+
+
+def total(hand):
   score = 0
   for card in hand:
     if card == "J" or card == "Q" or card == "K":
@@ -36,8 +49,6 @@ def total():
       score += card 
   return score
 
-player_score = total()
-print(player_score)
 
 def play_again():
   again = input("もう１度プレイしますか？　(Y/N): ")
@@ -48,3 +59,14 @@ def play_again():
     print("お疲れ様でした！")
     exit()
 
+
+def game():
+  dealer_hand = deal()
+  player_hand = deal()
+  print(f"ディーラーは{dealer_hand[0]}です")
+  print(f"プレイヤーは{player_hand}です")
+  print(total(dealer_hand))
+  print(total(player_hand))
+
+
+game()
